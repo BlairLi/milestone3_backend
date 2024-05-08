d41f3102f5eba823fcde3b9ca732984e0b31f5da
BlairLi <1579193739@qq.com>
2024-05-08 16:43:19 -0400

    Changes to be committed: 	modified:   .env 	modified:   package.json 	modified:   server.js

--- server.js
+++ server.js
@@ -58,7 +58,8 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const PORT = 5001;
+// const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);

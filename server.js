ea6ba78fba54a543cdeec276868731210fe5477a
BlairLi <1579193739@qq.com>
2024-05-08 20:43:22 -0400

    Changes to be committed: 	new file:   404.html 	modified:   server.js

--- server.js
+++ server.js
@@ -49,16 +49,27 @@ app.use('/api', userRoutes);
 app.use('/api', categoryRoutes);
 app.use('/api', productRoutes);
 
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('build'));
-  // app.use(express.static('client/build'));
+// if (process.env.NODE_ENV === 'production') {
+//   // Set static folder
+//   app.use(express.static('build'));
+//   // app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-    // res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+//     // res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
+
+app.all('*', (req, res) => {
+  res.status(404);
+  if (req.accepts('html')) {
+      res.sendFile(path.join(__dirname, '404.html'));
+  } else if (req.accepts('json')) {
+      res.json({ "error": "404 Not Found" });
+  } else {
+      res.type('txt').send("404 Not Found");
+  }
+});
 
 // const PORT = 5001;
 const PORT = process.env.PORT || 5001;

2cfe37e3591fd06b52d0d5c0c34da7507adb53b7
BlairLi <1579193739@qq.com>
2024-05-08 20:06:24 -0400

    Changes to be committed: 	modified:   server.js

--- server.js
+++ server.js
@@ -49,14 +49,14 @@ app.use('/api', userRoutes);
 app.use('/api', categoryRoutes);
 app.use('/api', productRoutes);
 
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+// if (process.env.NODE_ENV === 'production') {
+//   // Set static folder
+//   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
 
 // const PORT = 5001;
 const PORT = process.env.PORT || 5001;
